9e50385f617c45b3dc00fc6a8e4f83ab25ff4dd8
Luís Henrique <83787208+luishfontes@users.noreply.github.com>
2025-02-11 07:14:30 -0300

    coloquei um exemplo mais agradável de conversão

--- aulas/exercicios/conversao_teste1.py
+++ aulas/exercicios/conversao_teste1.py
@@ -1,6 +1,11 @@
-idade = input('Digite a sua idade: \n')
-idade = int(idade)
+numero = input('Digite um número: \n')
+print('tipo atual do número:')
+print(type(numero))
 
-print(f'Sua idade é {idade}')
+numero_convertido = int(numero)
+print('novo tipo: ')
+print(type(numero_convertido))
+
+print(f'Seu número {numero_convertido}, foi convertido para inteiro')
 
 # Nesse teste a idade não pode ser um caractere, apenas números inteiros.
